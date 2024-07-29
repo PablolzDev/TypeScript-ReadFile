c5a3c498c1694d54fd3f16de3f57ba4e606a9d12
PablolzDev <pabloinprogramacion@gmail.com>
2024-07-28 19:20:11 -0500

    Models and controllers

---
+++ src/controllers/csv.controller.ts
@@ -0,0 +1,18 @@
+import Papa from 'papaparse';
+import { DataEntry } from "../models/csv.model";
+
+// Función para analizar el archivo CSV y retornar los datos como una promesa
+export const processCSVFile = (csvFile: File): Promise<DataEntry[]> => {
+    return new Promise((resolvePromise, rejectPromise) => {
+        Papa.parse(csvFile, {
+            header: true, // Considera la primera fila como encabezados
+            skipEmptyLines: true, // Ignora las líneas en blanco
+            complete: (parseResults) => {
+                resolvePromise(parseResults.data as DataEntry[]); // Resuelve la promesa con los datos analizados
+            },
+            error: (parseError) => {
+                rejectPromise(parseError); // Rechaza la promesa si ocurre un error
+            },
+        });
+    });
+};
---
+++ src/models/csv.model.ts
@@ -0,0 +1,4 @@
+// Definición de la interfaz para las entradas de datos
+export interface DataEntry {
+    [columnName: string]: string; // Clave de tipo string (nombre de columna) y valor de tipo string (valor de celda)
+}
